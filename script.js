e4e8d2a7539230c8f09b97948a4b50807d621ef1
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-04 01:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function volume_sphere(event) {
-    // Prevent the form from submitting normally
+   
     event.preventDefault();
 
-    // Get the radius input value
+
     const radiusInput = document.getElementById('radius').value;
     const radius = parseFloat(radiusInput);
 
@@ -15,7 +15,7 @@ function volume_sphere(event) {
     const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
     // Update the volume input field with the result
-    document.getElementById('volume').value = volume.toFixed(2);
+    document.getElementById('volume').value = volume.toFixed(4);
 }
 
 
